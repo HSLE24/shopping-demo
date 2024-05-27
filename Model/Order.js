6c82d49b1dbc289f24b38db1ff310098bb7d44df
unknown <sh911124@gmail.com>
2024-05-28 00:18:08 +0900

    first commit: db schema

---
+++ Model/Order.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+const orderSchema = Schema(
+  {
+    userId: { type: mongoose.ObjectId, ref: User },
+    shipTo: { type: Object, required: true },
+    contact: { type: Object, required: true },
+    totalPrice: { type: Number, required: true },
+    status: { type: String, default: "preparing" },
+    orderNum: { type: String },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product },
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1 },
+        price: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+cartSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updatedAt;
+  return obj;
+};
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
